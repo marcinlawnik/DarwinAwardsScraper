af8bd6a16d77d08a7195129ae59c689d346f09c6
Marcin Lawniczak <marcin.safmb@wp.pl>
2014-01-07 16:46:34 +0100

    Added some more code

--- scraper.php
+++ scraper.php
@@ -7,3 +7,27 @@
  * To change this template use File | Settings | File Templates.
  */
 
+require('simple_html_dom.php');
+
+$currentUrl='http://darwinawards.com/darwin/darwin1993-02.html';
+
+$allUrl = array();
+$isNext=TRUE;
+
+while($isNext){//there is next link
+$allUrl[] .= $currentUrl;
+$currentPage=file_get_html($currentUrl);
+$currentStory=$currentPage->find('td [bgcolor=#FFFFFF]')->plaintext;
+echo $currentStory;
+if(isset($currentPage->find('td [bgcolor=#FFFFFF]'))){
+   $currentUrl='http://darwinawards.com/darwin/';
+   $allLinks=$currentPage->find('a');
+   foreach($allLinks as $link){
+       if(isset($link->find('img [width=61]'))){
+           $currentUrl = $link->href;
+       }
+   }
+}else{
+    $isNext=FALSE;
+}
+}
